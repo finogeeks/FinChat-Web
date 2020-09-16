c479ec6693e2ec8d5bf342606c56b60259e2f1db
[lizhipeng] <[lizhipeng@finogeeks.com]>
2020-09-16 13:57:24 +0800

    feat: del dist

--- vue.config.js
+++ vue.config.js
@@ -3,47 +3,47 @@ const path = require('path');
 
 console.log('==================process.env.NODE_ENV==================');
 console.log(process.env.NODE_ENV);
-// const target = 'https://api.finogeeks.club';
-const target = 'https://finchat-dev.finogeeks.club';
+const target = 'https://api.finogeeks.club';
+// const target = 'https://finchat-dev.finogeeks.club';
 // const target = 'http://fino.qhee.com.cn';
 // const target = 'https://testimchat.gf.com.cn';
 
 module.exports = {
-    configureWebpack: {
-        resolve: {
-            alias: {
-                '@': path.join(__dirname, './src'),
-            },
-        },
-        devServer: {
-            proxy: {
-                '/api': {
-                    target: target,
-                    changeOrigin: true,
-                    ws: true,
-                },
-                '/_matrix': {
-                    target: target,
-                    changeOrigin: true,
-                    ws: true,
-                },
-                '/auth': {
-                    target: 'https://iam3.finogeeks.club:7777',
-                    changeOrigin: true,
-                    ws: true,
-                },
-            }
-        }
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.join(__dirname, './src'),
+      },
     },
-    publicPath: process.env.NODE_ENV === 'production'
+    devServer: {
+      proxy: {
+        '/api': {
+          target,
+          changeOrigin: true,
+          ws: true,
+        },
+        '/_matrix': {
+          target,
+          changeOrigin: true,
+          ws: true,
+        },
+        '/auth': {
+          target: 'https://iam3.finogeeks.club:7777',
+          changeOrigin: true,
+          ws: true,
+        },
+      },
+    },
+  },
+  publicPath: process.env.NODE_ENV === 'production'
     ? './'
     : '/',
-    productionSourceMap:false, 
-    css: {
-        loaderOptions: {
-            less: {
-                javascriptEnabled: true,
-            }
-        }
-    }
-}
+  productionSourceMap: false,
+  css: {
+    loaderOptions: {
+      less: {
+        javascriptEnabled: true,
+      },
+    },
+  },
+};
