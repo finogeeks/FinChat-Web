e85cd13f270e0b1fefaa1c0baccacd4b650ffa7e
joker <9inedust@gmail.com>
2020-09-23 14:58:41 +0800

    Fix/optimization (#3)
    * feat: change tsconfig
    
    * feat: add gitignore
    
    * feat: del dist
    
    * fix: change editor clear time
    
    * fix bugs
    
    * fix: add new mute
    
    * fix: change unread time
    
    * fix: change Notification option
    
    * fix: del log
    
    Co-authored-by: [lizhipeng] <[lizhipeng@finogeeks.com]>

--- src/App.vue
+++ src/App.vue
@@ -53,41 +53,53 @@ export default {
       this.$store.commit('updateRoomList', cloneDeep(newRoom));
     });
     emitter.addListener('NEW_MSG_ALERT', async (mxEvent, newRoom) => {
-      // console.log('NEW_MSG_ALERT');
-      // console.log(mxEvent, newRoom);
-      // const sender = newRoom.members.find(e => {
-      //   return e.userId === newRoom.lastMessage.senderId
-      // });
-      // const event = mxEvent.event;
+      // console.log(mxEvent, '来消息了');
+      // console.log(this.$store);
       const msgContent = mxEvent.event.content;
       const iamInfo = JSON.parse(window.localStorage.getItem(IAM_INFO_KEY));
       const userId = iamInfo ? iamInfo.user_id : null;
       const sender = mxEvent.sender.userId;
       // // console.log(sender, userId);
-      const otherSender = sender !== userId;
       const hasMsgType = msgContent.msgtype;
-      const msgCanAlert = Object.keys(MSG_TYPE_ALERT).some(e => MSG_TYPE_ALERT[e] === msgContent.msgtype);
       const isMute = newRoom.isMute;
       const alertMe = msgContent.idlist && msgContent.idlist.indexOf(userId) > -1;
-      // console.log('whether alert ? ');
-      // console.log(otherSender, hasMsgType, msgCanAlert, isMute);
-      if ((otherSender && hasMsgType && msgCanAlert && !isMute) || (isMute && msgContent.msgtype === MSG_TYPE.alert && alertMe)) {
+      if (!mxEvent.event.type === 'm.room.message') return; // 房间消息类型
+      if (!mxEvent.event.content) return;
+      if (!mxEvent.event.content.msgtype) return;
+      const msgtype = mxEvent.event.content.msgtype; // 类型字段
+      if (sender === userId) return; // 本人消息过滤
+      const msgCanAlert = Object.keys(MSG_TYPE_ALERT).some(e => MSG_TYPE_ALERT[e] === msgtype);
+      if (!msgCanAlert) return;
+      if (!isMute || (isMute && msgContent.msgtype === MSG_TYPE.alert && alertMe)) {
         if (Notification.permission !== 'default') {
           window.focus();
           await Notification.requestPermission();
         }
-        const iconurl = window.matrix.user.mxcTransfer((sender && sender.avatarUrl) || '');
+        let sender = '';
+        let body = mxEvent.event.content.body;
+        if (MSG_TYPE.file === msgtype) body = `[文件]${mxEvent.event.content.body}`;
+        if (MSG_TYPE.url === msgtype) body = `[链接]${mxEvent.event.content.info.title || mxEvent.event.content.info.description}`;
+        if (MSG_TYPE.video === msgtype) body = '[视频]';
+        if (MSG_TYPE.audio === msgtype) body = '[语音]';
+        if (MSG_TYPE.location === msgtype) body = '[位置]';
+        if (!newRoom.isDirect) {
+          sender = `${mxEvent.sender.name} :`;
+        }
+        if (MSG_TYPE.alert === msgtype) {
+          sender = '';
+          body = `[有人@我]${mxEvent.sender.name} :${mxEvent.event.content.body}`;
+        }
+
         const notification = new Notification(newRoom.name, {
           dir: 'auto',
           lang: 'hi',
-          body: `${newRoom.isDirect ? '' : (`${mxEvent.sender.name}: `)}${newRoom.lastMessage.msgBody}`,
-          icon: iconurl,
+          tag: '',
+          body: `${sender}${body}`,
+          icon: window.matrix.user.mxcTransfer((sender && sender.avatarUrl) || ''),
           silent: true,
           sound: 'http://data.huiyi8.com/yinxiao/mp3/73913.mp3',
         });
-        // // console.log(notification);
         notification.onclick = () => {
-          // // console.log('notification.onclick');
           window.focus();
           if (this.$refs.chatPage && this.$refs.chatPage.enterRoom && !this.$refs.chatPage.whetherSameRoom(mxEvent.sender.roomId)) {
             this.$refs.chatPage.enterRoom(newRoom);
@@ -95,6 +107,30 @@ export default {
           notification.close();
         };
       }
+      // if ((hasMsgType && msgCanAlert && !isMute) || (isMute && msgContent.msgtype === MSG_TYPE.alert && alertMe)) {
+      //   if (Notification.permission !== 'default') {
+      //     window.focus();
+      //     await Notification.requestPermission();
+      //   }
+      //   const iconurl = window.matrix.user.mxcTransfer((sender && sender.avatarUrl) || '');
+      //   const notification = new Notification(newRoom.name, {
+      //     dir: 'auto',
+      //     lang: 'hi',
+      //     body: `${newRoom.isDirect ? '' : (`${mxEvent.sender.name}: `)}${newRoom.lastMessage.msgBody}`,
+      //     icon: iconurl,
+      //     silent: true,
+      //     sound: 'http://data.huiyi8.com/yinxiao/mp3/73913.mp3',
+      //   });
+      //   // // console.log(notification);
+      //   notification.onclick = () => {
+      //     // // console.log('notification.onclick');
+      //     window.focus();
+      //     if (this.$refs.chatPage && this.$refs.chatPage.enterRoom && !this.$refs.chatPage.whetherSameRoom(mxEvent.sender.roomId)) {
+      //       this.$refs.chatPage.enterRoom(newRoom);
+      //     }
+      //     notification.close();
+      //   };
+      // }
     });
     emitter.addListener('USERS_INIT', (usermap) => {
       // console.log('USERS_INIT');
