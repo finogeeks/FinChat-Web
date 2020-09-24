c13344e9768b53e3ad617252d8397271f8d7353c
[lizhipeng] <[lizhipeng@finogeeks.com]>
2020-09-24 16:09:39 +0800

    fix: del ""

--- config/run.sh
+++ config/run.sh
@@ -1,2 +1,2 @@
 /bin/sh ./config.sh
-/bin/sh -c "DOLLAR='$' "envsubst < /app/nginx.conf.template > /etc/nginx/conf.d/default.conf && exec nginx -g 'daemon off;'"
+/bin/sh -c "DOLLAR='$' envsubst < /app/nginx.conf.template > /etc/nginx/conf.d/default.conf && exec nginx -g 'daemon off;'"
